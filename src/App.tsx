d2d33626ce5ac201df4abd35a923a96c3ba2c152
Adam Poukar <adam.poukar@ceskesperky.cz>
2025-12-03 23:56:27 +0100

    Added contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import TbThemeProvider from 'components/TbThemeProvider'
 import AboutMe from 'views/AboutMe'
+import Contacts from 'views/Contacts'
 import Menu from 'views/Menu'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <TbThemeProvider>
         <Menu />
         <AboutMe />
+        <Contacts />
       </TbThemeProvider>
     </>
   )
---
+++ src/views/Contacts.tsx
@@ -0,0 +1,42 @@
+import { Link, styled, Typography, type LinkProps, type TypographyProps } from "@mui/material";
+import TbHeadline from "components/TbHeadline";
+
+const ContactTypography = styled(Typography)<TypographyProps>(({ theme }) => ({
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+const ContactLink = styled(Link)<LinkProps>(() => ({
+    textAlign: 'center',
+    width: '100%',
+    display: 'block'
+}))
+
+export default function Contacts() {
+    return (
+        <>
+            <TbHeadline heading="Kontakt" />
+            <ContactTypography>
+                TB Welding - Tomáš Bičej
+            </ContactTypography>
+            <ContactTypography>
+                Ostrava – Poruba
+            </ContactTypography>
+            <ContactLink href="tel:+420775492685" margin="1% 0 0 0">
+                +420 775 492 685
+            </ContactLink>
+            <ContactLink href="mailto:tbwelding@seznam.cz">
+                tbwelding@seznam.cz
+            </ContactLink>
+            <ContactTypography>
+                IČO: 000000000
+            </ContactTypography>
+            <ContactTypography margin="1% 0 0 0">
+                Zakázky po celé ČR - po domluvě
+            </ContactTypography>
+            <ContactTypography>
+                Možnost realizace zakázek i v zahraničí
+            </ContactTypography>
+        </>
+    )
+}
